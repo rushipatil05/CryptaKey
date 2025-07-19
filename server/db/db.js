9faece9d1b459360cf680b7efbdab88117ff3d38
Rushikesh Dilip Patil <rushipatil12448@gmail.com>
2025-07-19 22:09:04 +0530

    modifying db

--- server/db/db.js
+++ server/db/db.js
@@ -4,10 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ Connected to MongoDB");
   } catch (error) {
     console.log("❌ Error connecting to MongoDB:", error.message);
