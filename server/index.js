598f3c270f3b7830cfdd9290971c68394a22cc3f
Rushikesh Dilip Patil <rushipatil12448@gmail.com>
2025-07-20 21:11:08 +0530

    Updated indexjs

--- server/index.js
+++ server/index.js
@@ -1,19 +1,18 @@
-import express from 'express';
-import cors from 'cors';
-import connectToMongoDB from './db/db.js';
+import express from 'express'
+import cors from 'cors'
+import connectToMongoDB from './db/db.js'
 
-import authRouter from './routes/auth.js';
-import noteRouter from './routes/note.js';
+import authRouter from './routes/auth.js'
+import noteRouter from './routes/note.js'
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use('/api/auth', authRouter);
-app.use('/api/note', noteRouter);
 
-// Connect to MongoDB immediately
-connectToMongoDB();
+const app=express()
+app.use(cors())
+app.use(express.json())
+app.use('/api/auth',authRouter)
+app.use('/api/note',noteRouter)
 
-// ✅ Don't call app.listen
-export default app;
-
+app.listen(5000,()=>{
+    connectToMongoDB()
+    console.log("server is running..")
+})
