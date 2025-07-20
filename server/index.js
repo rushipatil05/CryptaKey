27ee3e8399887b35bd4498853ac14df204107f19
Rushikesh Dilip Patil <rushipatil12448@gmail.com>
2025-07-20 20:15:45 +0530

    Updating packagejson

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
-import express from 'express'
-import cors from 'cors'
-import connectToMongoDB from './db/db.js'
+import express from 'express';
+import cors from 'cors';
+import connectToMongoDB from './db/db.js';
 
-import authRouter from './routes/auth.js'
-import noteRouter from './routes/note.js'
+import authRouter from './routes/auth.js';
+import noteRouter from './routes/note.js';
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRouter);
+app.use('/api/note', noteRouter);
 
-const app=express()
-app.use(cors())
-app.use(express.json())
-app.use('/api/auth',authRouter)
-app.use('/api/note',noteRouter)
+// Connect to MongoDB immediately
+connectToMongoDB();
 
-app.listen(5000,()=>{
-    connectToMongoDB()
-    console.log("server is running..")
-})
+// ✅ Don't call app.listen
+export default app;
