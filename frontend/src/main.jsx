d77785a114af87317bc333d58131e9667b05edd5
Rushikesh Patil <142619840+rushipatil05@users.noreply.github.com>
2025-07-22 14:20:48 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ContextProvider from './context/ContextProvider.jsx'
+import { createBrowserRouter } from 'react-router'
+import FrontPage from './pages/FrontPage.jsx'
+import { Home } from 'lucide-react'
+import Signup from './pages/Signup.jsx'
+import Login from './pages/Login.jsx'
+import { RouterProvider } from 'react-router-dom'
+
+
+const router = createBrowserRouter([
+  { path: '/', element: <FrontPage /> },
+  { path: '/home', element: <Home /> },
+  { path: '/register', element: <Signup /> },
+  { path: '/login', element: <Login /> }
+]);
+
 createRoot(document.getElementById('root')).render(
+
+
   <ContextProvider>
-    <App />
+    <RouterProvider router={router} />
   </ContextProvider>,
 )
